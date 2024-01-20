1bc8943b5a1fd7481dd68fc978e36650d1908dc9
zigu <porscha@pincservices.de>
2024-01-20 20:44:23 +0100

    fix problems with ts compiler (after react 18 upgrade), add end-of-life plugin

--- packages/app/src/components/catalog/contents/overview/ComponentOverview.tsx
+++ packages/app/src/components/catalog/contents/overview/ComponentOverview.tsx
@@ -1,91 +1,114 @@
-import { Grid } from '@material-ui/core';
+import {Grid} from '@material-ui/core';
 import {
-    EntityAboutCard,
-    EntityLinksCard,
-    EntitySwitch
+  EntityAboutCard, EntityLabelsCard,
+  EntityLinksCard,
+  EntitySwitch, hasLabels
 } from "@backstage/plugin-catalog";
 import React from "react";
 import {entityWarningContent} from "../EntityWarning";
 import {EntitySonarQubeCard} from "@backstage/plugin-sonarqube";
 import {isSonarQubeAvailable} from "@backstage/plugin-sonarqube-react";
-import {EntityGitlabLanguageCard, EntityGitlabReleasesCard, isGitlabAvailable} from "@immobiliarelabs/backstage-plugin-gitlab";
+import {
+  EntityGitlabLanguageCard,
+  EntityGitlabReleasesCard,
+  isGitlabAvailable
+} from "@immobiliarelabs/backstage-plugin-gitlab";
 import {EntityJiraOverviewCard, isJiraAvailable} from "@roadiehq/backstage-plugin-jira";
 import {EntityArgoCDStatusCard, isArgocdAvailable} from "@internal/plugin-pincservices-argocd";
 import {areLinksAvailable, isChangelogFileRefAvailable} from "../../../../lib/conditions";
 import {EntityChangelogCard} from "@rsc-labs/backstage-changelog-plugin";
+import {EntityEndOfLifeCard, isEndOfLifeAvailable} from "@dweber019/backstage-plugin-endoflife";
 
 export const componentOverviewContent = (
-    <Grid container spacing={3}>
-        <Grid item xs={12}>
-            {entityWarningContent}
-        </Grid>
-
-        <Grid item container spacing={3} xs={12} md={5} lg={5} >
-            { /* Generic info section */ }
-            <EntitySwitch>
-                <EntitySwitch.Case if={areLinksAvailable}>
-                    <Grid item xs={12}>
-                        <EntityLinksCard />
-                    </Grid>
-                </EntitySwitch.Case>
-            </EntitySwitch>
-            <Grid item xs={12}>
-                <EntityAboutCard />
-            </Grid>
-
-            <EntitySwitch>
-                <EntitySwitch.Case if={isGitlabAvailable}>
-                    <Grid item xs={12}>
-                        <EntityGitlabLanguageCard />
-                    </Grid>
-                </EntitySwitch.Case>
-            </EntitySwitch>
-
-            { /* Quality section */ }
-            <EntitySwitch>
-                <EntitySwitch.Case if={isSonarQubeAvailable}>
-                    <Grid item xs={12}>
-                        <EntitySonarQubeCard variant="gridItem" />
-                    </Grid>
-                </EntitySwitch.Case>
-            </EntitySwitch>
-
-        </Grid>
-
-        <Grid item container spacing={3} xs={12} md={7} lg={7}>
-            { /* Releases section */ }
-            <EntitySwitch>
-                <EntitySwitch.Case if={isArgocdAvailable}>
-                    <Grid item xs={12}>
-                        <EntityArgoCDStatusCard />
-                    </Grid>
-                </EntitySwitch.Case>
-            </EntitySwitch>
-
-            <EntitySwitch>
-                <EntitySwitch.Case if={isGitlabAvailable}>
-                    <Grid item xs={12}>
-                        <EntityGitlabReleasesCard />
-                    </Grid>
-                </EntitySwitch.Case>
-            </EntitySwitch>
-
-            <EntitySwitch>
-                <EntitySwitch.Case if={isChangelogFileRefAvailable}>
-                    <Grid item xs={12}>
-                        <EntityChangelogCard />
-                    </Grid>
-                </EntitySwitch.Case>
-            </EntitySwitch>
-
-            { /* Issue tracking section */ }
-            <EntitySwitch>
-                <EntitySwitch.Case if={isJiraAvailable}>
-                    <Grid item xs={12}>
-                        <EntityJiraOverviewCard />
-                    </Grid>
-                </EntitySwitch.Case>
-            </EntitySwitch>
-        </Grid>
+  <Grid container spacing={3}>
+    <Grid item xs={12}>
+      {entityWarningContent}
     </Grid>
+
+    <Grid item container spacing={3} xs={12} md={5} lg={5}>
+      { /* Generic info section */}
+      <EntitySwitch>
+        <EntitySwitch.Case if={areLinksAvailable}>
+          <Grid item xs={12}>
+            <EntityLinksCard/>
+          </Grid>
+        </EntitySwitch.Case>
+      </EntitySwitch>
+
+      <Grid item xs={12}>
+        <EntityAboutCard/>
+      </Grid>
+
+      <EntitySwitch>
+        <EntitySwitch.Case if={hasLabels}>
+          <Grid item xs={12}>
+            <EntityLabelsCard/>
+          </Grid>
+        </EntitySwitch.Case>
+      </EntitySwitch>
+
+      <EntitySwitch>
+        <EntitySwitch.Case if={isGitlabAvailable}>
+          <Grid item xs={12}>
+            <EntityGitlabLanguageCard/>
+          </Grid>
+        </EntitySwitch.Case>
+      </EntitySwitch>
+
+      { /* Quality section */}
+      <EntitySwitch>
+        <EntitySwitch.Case if={isSonarQubeAvailable}>
+          <Grid item xs={12}>
+            <EntitySonarQubeCard variant="gridItem"/>
+          </Grid>
+        </EntitySwitch.Case>
+      </EntitySwitch>
+
+      { /* End of Life section */}
+      <EntitySwitch>
+        <EntitySwitch.Case if={isEndOfLifeAvailable}>
+          <Grid item xs={12}>
+            <EntityEndOfLifeCard />
+          </Grid>
+        </EntitySwitch.Case>
+      </EntitySwitch>
+
+    </Grid>
+
+    <Grid item container spacing={3} xs={12} md={7} lg={7}>
+      { /* Releases section */}
+      <EntitySwitch>
+        <EntitySwitch.Case if={isArgocdAvailable}>
+          <Grid item xs={12}>
+            <EntityArgoCDStatusCard/>
+          </Grid>
+        </EntitySwitch.Case>
+      </EntitySwitch>
+
+      <EntitySwitch>
+        <EntitySwitch.Case if={isGitlabAvailable}>
+          <Grid item xs={12}>
+            <EntityGitlabReleasesCard/>
+          </Grid>
+        </EntitySwitch.Case>
+      </EntitySwitch>
+
+      <EntitySwitch>
+        <EntitySwitch.Case if={isChangelogFileRefAvailable}>
+          <Grid item xs={12}>
+            <EntityChangelogCard/>
+          </Grid>
+        </EntitySwitch.Case>
+      </EntitySwitch>
+
+      { /* Issue tracking section */}
+      <EntitySwitch>
+        <EntitySwitch.Case if={isJiraAvailable}>
+          <Grid item xs={12}>
+            <EntityJiraOverviewCard/>
+          </Grid>
+        </EntitySwitch.Case>
+      </EntitySwitch>
+    </Grid>
+  </Grid>
 );
